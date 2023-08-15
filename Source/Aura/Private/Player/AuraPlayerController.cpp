4f07175babbc4d2730da3bcd5196a3960ad0220e
chris UN <codemedarkone@gmail.com>
2023-08-14 20:57:14 -0600

    added the context and subsystem to get localplayer. I also added a few mousecursor settings and input modes

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -2,4 +2,40 @@
 
 
 #include "Player/AuraPlayerController.h"
+#include "InputMappingContext.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "Components/InputComponent.h"
 
+
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true; 
+
+
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay(); 
+
+	check(AuraContext); 
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem); 
+	Subsystem->AddMappingContext(AuraContext, 0); 
+
+	bShowMouseCursor = true; 
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	//we can use input from keyboard and mouse and to use inputs to affect ui such as widgets
+	FInputModeGameAndUI InputModeData;
+	//mouse wont be locked to viewport
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	//Once cursor is captured in viewport it wont hide it
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData); 
+
+
+}
