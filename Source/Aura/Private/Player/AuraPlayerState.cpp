821083909959cd9d5da0c70aa7d5353c57a2b074
chris UN <codemedarkone@gmail.com>
2023-08-15 17:39:48 -0600

    Set AbilitySystemComponent "SetReplicationMode" to mixed for playerstate for multipleayer which you can play single player as well. The Full mode is for singleplayer only, you cant do multiplayer if built as full. I set the AuraEnemy to minimal since its AI.

--- Source/Aura/Private/Player/AuraPlayerState.cpp
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -8,6 +8,7 @@ AAuraPlayerState::AAuraPlayerState()
 {
 	AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
 	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Mixed); 
 
 	AttributeSet = CreateDefaultSubobject<UAttributeSet>("AttributeSet");
 
