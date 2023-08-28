8826070ac7008958c9ee9a89a03f3e6aace01fc3
chris UN <codemedarkone@gmail.com>
2023-08-28 15:11:48 -0600

    8-15 Attribute Menu WidgetController: creeated the class AttributeMenuWidgetController based of the aurauserwidget. I initialized the inherited functions bindcallbackstodependencies() and broadcastinitialvalues().

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+// CopyRight Red Project
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+
+}
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+
+}
