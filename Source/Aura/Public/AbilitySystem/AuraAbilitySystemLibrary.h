1d480ce1d1568284037aba72b2b76b8544fdbd66
chris UN <codemedarkone@gmail.com>
2023-08-28 15:42:18 -0600

    8-16 Aura Ability System Blueprint Library: Created new class AuraBilitySystemLibrary which is my blueprint function library. Created a function GetOverlayWidgetController(UObject* WorldContextObject) and got all 4 required pieces of information. PlayerState, ASC, UAttributeSet, FWidgetControllerParams which holds aall 4 of these.

---
+++ Source/Aura/Public/AbilitySystem/AuraAbilitySystemLibrary.h
@@ -0,0 +1,24 @@
+// CopyRight Red Project
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "AuraAbilitySystemLibrary.generated.h"
+
+
+class UOverlayWidgetController; 
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraAbilitySystemLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+public:
+
+
+	UFUNCTION(BlueprintPure, Category = "AuraAbilitySystemLibrary|WidgetController")
+	static UOverlayWidgetController* GetOverlayWidgetController(const UObject* WorldContextObject); 
+};
