76017c125a4e6b027371ccdbcd47eed4a887d48f
chris UN <codemedarkone@gmail.com>
2023-09-09 17:16:25 -0600

    9-14 Aura ProjectileSpell: I created a new class AuraProjectileSpell inherited(Derived from) my AuraGameplayabilitybase class that I made in C++ which makes this a child. I then add the ActivateAbility() function which gas has and added a UKismetSystemLibrary::PrintString() function to print "activate ability" in this activateability() function. Then I created a blueprint GA class based on this class(AuraProjectileSpell) and added the startup tag to LMB in the blueprint. Then once in PIE, clicking on the goblin showed blueprint activate first then C++.

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -0,0 +1,13 @@
+// CopyRight Red Project
+
+
+#include "AbilitySystem/Abilities/AuraProjectileSpell.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	UKismetSystemLibrary::PrintString(this, FString("ActivateAbillity (C++)"), true, true, FLinearColor::Yellow, 3.f); 
+
+}
---
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraProjectileSpell.h
@@ -0,0 +1,22 @@
+// CopyRight Red Project
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/AuraGameplayAbilityBase.h"
+#include "AuraProjectileSpell.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraProjectileSpell : public UAuraGameplayAbilityBase
+{
+	GENERATED_BODY()
+	
+
+protected:
+
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData); 
+
+};
