7ae50d42a00b46308c9e1b487ded67f75a532aa2
chris UN <codemedarkone@gmail.com>
2023-09-10 16:32:33 -0600

    10-4 Custom Ability Task: Created a custom AbilityTask class derived from abilitytask. I created a static UTargetDataUnderMouse* CreateTargetDataUnderMouse(UGameplayAbility* OwningAbility) function and added many specifiers to it which is what i guess is required to run it in blueprint. In BP GA_Firebolt event graph when I search this function under its display name TargetDataUnderMouse I see it has a valid data which is the FMouseTargetDataSignature ValidData; variable I created for the delegate I made. It shows ValidData Pin and the Data pin which I gave it in the delegate DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMouseTargetDataSignature, const FVector&, Data). I used a debug sphere when i clicked on the goblin enemy. However on the servier the debug sphere appeared in (0,0,0) position on client when used which needs to be fixed.

---
+++ Source/Aura/Private/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
@@ -0,0 +1,22 @@
+// CopyRight Red Project
+
+
+#include "AbilitySystem/AbilityTasks/TargetDataUnderMouse.h"
+
+
+UTargetDataUnderMouse* UTargetDataUnderMouse::CreateTargetDataUnderMouse(UGameplayAbility* OwningAbility)
+{
+	UTargetDataUnderMouse* MyObj = NewAbilityTask<UTargetDataUnderMouse>(OwningAbility);
+	return MyObj; 
+
+}
+
+void UTargetDataUnderMouse::Activate()
+{
+	APlayerController* PC = Ability->GetCurrentActorInfo()->PlayerController.Get();
+	FHitResult CursorHit;
+	PC->GetHitResultUnderCursor(ECollisionChannel::ECC_Visibility, false, CursorHit); 
+
+	ValidData.Broadcast(CursorHit.Location); 
+
+}
