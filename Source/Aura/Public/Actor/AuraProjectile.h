aed73dca57d1c987f05a9a5ff3ca3ca186ed3d9b
chris UN <codemedarkone@gmail.com>
2023-09-17 17:09:29 -0600

    section 10-11 Projectile Impact: I added play sounds and Niagara effects in Projectile class. I added a notify in abp for sound as well.

--- Source/Aura/Public/Actor/AuraProjectile.h
+++ Source/Aura/Public/Actor/AuraProjectile.h
@@ -8,6 +8,8 @@
 
 class USphereComponent;
 class UProjectileMovementComponent;
+class UNiagaraSystem;
+class UAudioComponent;
 
 
 UCLASS()
@@ -21,16 +23,35 @@ public:
 	UPROPERTY(VisibleAnywhere)
 	TObjectPtr<UProjectileMovementComponent> ProjectileMovement; 
 
+
 protected:
 	virtual void BeginPlay() override;
+	virtual void Destroyed() override; 
 
 	UFUNCTION()
 	void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult); 
 
 private:	
 
+	UPROPERTY(EditDefaultsOnly)
+	float LifeSpan = 15.f; 
+
+	bool bHit = false; 
+
 	UPROPERTY(VisibleAnywhere)
 	TObjectPtr<USphereComponent> Sphere;
+
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UNiagaraSystem> ImpactEffect;
+
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<USoundBase> ImpactSound; 
+
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<USoundBase> LoopingSound;
+	
+	UPROPERTY()
+	TObjectPtr<UAudioComponent> LoopingSoundComponent; 
 
 public:	
 
