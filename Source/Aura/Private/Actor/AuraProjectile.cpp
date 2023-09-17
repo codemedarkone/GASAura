83ad7007cb78c0547ecc257fb3478c809f7c69ad
chris UN <codemedarkone@gmail.com>
2023-09-17 17:24:08 -0600

    section 10-12 Projectile Collision Channel: I set the collision channel for projectile as an object type in project settings. I set it as a tracechannel in aura.h. Only enemy is hit by projectile and pillar while potions th projectile flies through.

--- Source/Aura/Private/Actor/AuraProjectile.cpp
+++ Source/Aura/Private/Actor/AuraProjectile.cpp
@@ -2,6 +2,7 @@
 
 
 #include "Actor/AuraProjectile.h"
+#include "Aura/Aura.h"
 #include "Components/AudioComponent.h"
 #include "Components/SphereComponent.h"
 #include "GameFramework/ProjectileMovementComponent.h"
@@ -17,6 +18,8 @@ AAuraProjectile::AAuraProjectile()
 
 	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
 	SetRootComponent(Sphere); 
+
+	Sphere->SetCollisionObjectType(ECC_Projectile); 
 	Sphere->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
 	Sphere->SetCollisionResponseToAllChannels(ECR_Ignore);
 	Sphere->SetCollisionResponseToChannel(ECC_WorldDynamic, ECR_Overlap);
