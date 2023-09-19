a87991a118c200640cf1613533e7845a136b8084
chris UN <codemedarkone@gmail.com>
2023-09-19 12:58:59 -0600

    11-2 Character Class Info: I created a new C++ Class CharacterClassInfo that derives from DataAsset. I created an enum for the types of classes and I created a struct called FCharacterCLassDefaultInfo with a Tsubclassof<GameplayEffect> Primary Attributes. Then in the public section I created 2 more tsubclassof<Gameplayeffect> named secondaryattributes and vitalattributes. I then also created a TMAP<ECharacterClass, FCharacterClassDefaultInfo> CharacterClassInformation to combine the enum and struct. The function GetClassDefaultInfo returns a FCharacterClassDefaultInfo.

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+// CopyRight Red Project
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+	return CharacterClassInformation.FindChecked(CharacterClass); 
+}
