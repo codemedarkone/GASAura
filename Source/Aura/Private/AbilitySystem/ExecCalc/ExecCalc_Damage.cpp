25c2bb803b8c412391a5d64faab0c88b677b6109
chris UN <codemedarkone@gmail.com>
2023-10-11 16:00:36 -0600

    section 13 - 144 ExecCalcs - Capturing Attributes. I captured attributes.

--- Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
+++ Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -3,12 +3,33 @@
 
 #include "AbilitySystem/ExecCalc/ExecCalc_Damage.h"
 #include "AbilitySystemComponent.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+
+
+//struct to hold all the capture definitions
+//Raw struct, not adding F or USTRUCT. Only used in this class
+struct AuraDamageStatics
+{
+	DECLARE_ATTRIBUTE_CAPTUREDEF(Armor); 
+
+	AuraDamageStatics()
+	{
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, Armor, Target, false);
+	}
+};
+
+static const AuraDamageStatics& DamageStatics()
+{
+	static AuraDamageStatics DStatics; 
+	return DStatics; 
+}
 
 UExecCalc_Damage::UExecCalc_Damage()
 {
-
+	RelevantAttributesToCapture.Add(DamageStatics().ArmorDef); 
 }
 
+//heart of execution calculation
 void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
 {
 	const UAbilitySystemComponent* SourceASC = ExecutionParams.GetSourceAbilitySystemComponent();
@@ -18,4 +39,18 @@ void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecuti
 	const AActor* TargetAvatar = TargetASC ? TargetASC->GetAvatarActor() : nullptr; 
 
 	const FGameplayEffectSpec& Spec = ExecutionParams.GetOwningSpec(); 
+
+	const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+	FAggregatorEvaluateParameters EvaluationParameters;
+	EvaluationParameters.SourceTags = SourceTags;
+	EvaluationParameters.TargetTags = TargetTags; 
+
+	float Armor = 0.f; 
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmorDef, EvaluationParameters, Armor); 
+	Armor = FMath::Max<float>(0.f, Armor); 
+	++Armor;
+
+	const FGameplayModifierEvaluatedData EvaluatedData(DamageStatics().ArmorProperty, EGameplayModOp::Additive, Armor); 
+	OutExecutionOutput.AddOutputModifier(EvaluatedData);
 }
