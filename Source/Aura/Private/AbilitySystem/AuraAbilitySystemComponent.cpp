1c7aab3792dfececefab6d2cf4d42e60e904e9ca
chris UN <codemedarkone@gmail.com>
2023-10-19 17:41:34 -0600

    Fixed bug in the AASC class in the fuction AbilityActorInfoSet Where I set the OnGameplayEffectAppliedDelegateToSelf where I need to set the function to ClientEfefctApplied.

--- Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -7,7 +7,7 @@
 
 void UAuraAbilitySystemComponent::AbilityActorInfoSet()
 {
-	OnGameplayEffectAppliedDelegateToSelf.AddUObject(this, &UAuraAbilitySystemComponent::ClientEffectApplied_Implementation);
+	OnGameplayEffectAppliedDelegateToSelf.AddUObject(this, &UAuraAbilitySystemComponent::ClientEffectApplied);
 }
 
 void UAuraAbilitySystemComponent::AddCharacterAbilities(const TArray<TSubclassOf<UGameplayAbility>>& StartupAbilities)
