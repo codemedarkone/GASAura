9dacaba543c30d297d37e8d97944074668ee6a8e
chris UN <codemedarkone@gmail.com>
2023-10-19 14:20:52 -0600

    14 - 159 Mapping Damage Types to resistances: I created game tags for the resistances and created gameplay tags for Damage types. I also created a TMap<FGameplayTag, FGameplayTag> DamageTypesToResistances to compare tags.

--- Source/Aura/Public/AuraGameplayTags.h
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -33,6 +33,11 @@ public:
 	FGameplayTag Attributes_Secondary_MaxHealth;
 	FGameplayTag Attributes_Secondary_MaxMana;
 
+	FGameplayTag Attributes_Resistance_Fire;
+	FGameplayTag Attributes_Resistance_Lightning;
+	FGameplayTag Attributes_Resistance_Arcane;
+	FGameplayTag Attributes_Resistance_Physical;
+
 	FGameplayTag InputTag_LMB;
 	FGameplayTag InputTag_RMB;
 	FGameplayTag InputTag_1;
@@ -44,8 +49,13 @@ public:
 
 	FGameplayTag Damage; 
 	FGameplayTag Damage_Fire; 
+	FGameplayTag Damage_Lightning;
+	FGameplayTag Damage_Arcane;
+	FGameplayTag Damage_Physical;
 
-	TArray<FGameplayTag> DamageTypes; 
+
+	TMap<FGameplayTag, FGameplayTag> DamageTypesToResistances;
+
 
 
 private:
