41606aa8bd05693a19755de542b225069fd47377
chris UN <codemedarkone@gmail.com>
2024-01-18 16:50:57 -0700

    Section 16-182:have enemy damage auro and show floating text for them.

--- Source/Aura/Public/AbilitySystem/Abilities/AuraDamageGameplayAbility.h
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraDamageGameplayAbility.h
@@ -14,6 +14,10 @@ class AURA_API UAuraDamageGameplayAbility : public UAuraGameplayAbilityBase
 {
 	GENERATED_BODY()
 	
+public:
+
+	UFUNCTION(BlueprintCallable)
+	void CauseDamage(AActor* TargetActor);
 protected:
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
